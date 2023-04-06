2064fa46fb6cee2dc4df18c214b5cd9163dbf110
Lupu Daniel <dlupu666@gmail.com>
2023-04-06 13:01:43 +0300

    Task_27 Finished

--- Task_27/Program.cs
+++ Task_27/Program.cs
@@ -5,4 +5,21 @@
 9012 -> 12
 */
 
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Введите число:");
+        int number = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        Console.WriteLine($"Сумма цифр в числе: {sum}");
+    }
+}
 
