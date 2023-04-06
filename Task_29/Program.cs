bf0e14699fee7b93ed4d4c059092622ea8d2db4a
Lupu Daniel <dlupu666@gmail.com>
2023-04-06 13:04:12 +0300

    Task_29 Finished

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -1,2 +1,17 @@
-﻿
+﻿/*Задача 29: Напишите программу,
+которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
 
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        int[] numbers = new int[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        for (int i = 0; i < numbers.Length; i++) {
+            Console.WriteLine(numbers[i]);
+        }
+    }
+}
