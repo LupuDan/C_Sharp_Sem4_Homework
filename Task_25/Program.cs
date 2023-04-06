5a74490768a92530fd93fec7700d71a1e3548ffc
Lupu Daniel <dlupu666@gmail.com>
2023-04-06 12:58:10 +0300

    Task_25 Finished

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -4,3 +4,22 @@
 2, 4 -> 16
 */
 
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Введите число A:");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите число B:");
+        int b = int.Parse(Console.ReadLine());
+
+        int result = 1;
+
+        for (int i = 0; i < b; i++) {
+            result *= a;
+        }
+
+        Console.WriteLine($"{a} в степени {b} = {result}");
+    }
+}
